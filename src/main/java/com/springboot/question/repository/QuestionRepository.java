074109557eb05406eb6af0b54e1b2205fec78aa9
NamTaekHo <taekho1225@gmail.com>
2025-02-12 09:48:16 +0900

    Question Repository Query 작성

--- src/main/java/com/springboot/question/repository/QuestionRepository.java
+++ src/main/java/com/springboot/question/repository/QuestionRepository.java
@@ -4,7 +4,19 @@ import com.springboot.question.entity.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    Page<Question> findByQuestionStatus(Question.QuestionStatus questionStatus, Pageable pageable);
+    @Query("SELECT q FROM Question q WHERE q.questionStatus != 'QUESTION_DEACTIVATED'")
+    Page<Question> findAllQuestions(Pageable pageable);
+
+    @Query("SELECT q FROM Question q WHERE q.id = :questionId AND "
+    + "(q.visibility = 'QUESTION_PUBLIC' OR (q.visibility = 'QUESTION_SECRET' AND "
+     + "(q.member.memberId = :memberId OR :isAdmin = true)))")
+    Optional<Question> findQuestionByIdAndAccess(@Param("questionId") Long questionId,
+                                                 @Param("memberId") Long memberId,
+                                                 @Param("isAdmin") boolean isAdmin);
 }
